94babe710c6d5d4eae15fa011ac494547a29cbfc
Awesomeze <ezeawesomeeze@gmail.com>
2025-03-17 17:36:21 +0100

    testing stage update

--- SmartX/src/App.tsx
+++ SmartX/src/App.tsx
@@ -1,19 +1,23 @@
 
 import "./App.css"
-import Hero from './components/Hero';
+import Home from './components/Home';
 import About from "./components/About"
 import Navbar from "./components/Navbar"
 import Testimonial from "./components/Testimonial"
+import Trade from "./components/Trade"
+import Contact from "./components/Contact"
 
 
 
 const App = () => {
   return (
-    <main className=' flex flex-col items-center relative min-h-screen w-screen overflow-hidden'>
-   <div className=""><Navbar/></div>
-   <div className='min-w-screen overflow-x-hidden'><Hero/></div>
-  <div className='p-2 min-w-screen overflow-x-hidden'><About/></div> 
-  <div className='p-2 min-w-screen overflow-x-hidden'><Testimonial/></div> 
+    <main className='overflow-x-hidden bg-black/70 flex flex-col items-center relative min-h-screen w-screen overflow-hidden'>
+   <section  className=""><Navbar/></section>
+   <section id="home" className='w-screen overflow-x-hidden'><Home/></section>
+  <section id="about" className=' p-2 w-screen overflow-x-hidden'><About/></section> 
+  <section id="testimonial" className='p-2 w-screen overflow-x-hidden'><Testimonial/></section> 
+  <section id="trade" className='p-2 w-screen overflow-x-hidden'><Trade/></section> 
+  <section id="contact" className='p-2 w-screen overflow-x-hidden'><Contact/></section> 
    </main>
   )
 };
--- SmartX/src/components/Navbar.tsx
+++ SmartX/src/components/Navbar.tsx
@@ -1,17 +1,17 @@
 
-import { useRef, useState } from "react"
-const navContainerRef = useRef(null)
-const NavList= ['Profile', 'About','Testimonial','Trade','Contact']
+
+
+const NavList= ['Home', 'About','Testimonial','Trade','Contact']
     
 const Navbar = () => {
     
-    const [hasClicked, sethasClicked] = useState(false)
+    
  
   return (
-   <div ref={navContainerRef} className=" z-30 h-6 absolute items-center top-2 left-0 bg-black/50 justify-center items-center flex font-bold w-screen p-3 transition-all duration-500 ">
+   <div  className=" z-30 fixed h-10 absolute items-center top-1 left-0 bg-black/80 justify-center items-center flex font-bold w-screen p-3 transition-all duration-500 ">
     <header className="flex size-full items-center justify-between">
         {NavList.map((item) =>(
-            <a key={item} href={`#${item.toLowerCase()}`} className="hover:border-1 border-white rounded-lg shadow-white hover:shadow-lg cursor-pointer px-1 ">
+            <a key={item} href={`#${item.toLowerCase()}`} className=" rounded-lg  shadow-blue-200 hover:shadow-lg cursor-pointer px-1 text-white ">
                 {item}
             </a>
         ))}
